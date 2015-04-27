e35043c545432d581ff33a1006b620bc8198a7a6
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-27 18:03:52 +0800

    Initial Commit

---
+++ UIPageControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIPageControl
+//
+//  Created by Calvin Cheng on 27/4/15.
+//  Copyright (c) 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
