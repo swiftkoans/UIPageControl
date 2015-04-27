f98e6b1d4d8b72a2bcee51b4e8be94599af86f60
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-27 20:18:43 +0800

    Add protocols UIPageViewControllerDataSource and UIPageViewControllerDelegate and don't forget .gitignore

--- UIPageControl/ViewController.swift
+++ UIPageControl/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
+    let pageTitles = ["Title 1", "Title 2", "Title 3", "Title 4"]
+    var images = ["long3.png","long4.png","long1.png","long2.png"]
+    var count = 0
+    var pageViewController : UIPageViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! PageContentViewController).pageIndex!
+        if (index <= 0) {
+            return nil
+        }
+        index--
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! PageContentViewController).pageIndex!
+        index++
+        if(index >= self.images.count){
+            return nil
+        }
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
 
+    func viewControllerAtIndex(index : Int) -> UIViewController? {
+        if((self.pageTitles.count == 0) || (index >= self.pageTitles.count)) {
+            return nil
+        }
+        let pageContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageContentViewController") as! PageContentViewController
+        
+        pageContentViewController.imageName = self.images[index]
+        pageContentViewController.titleText = self.pageTitles[index]
+        pageContentViewController.pageIndex = index
+        return pageContentViewController
+    }
 }
 
